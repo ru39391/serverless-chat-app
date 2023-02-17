0be912b085d1b824184c618e1eae1f33b1a4db93
ru39391 <dev@39391.ru>
2023-02-17 12:43:03 +0300

    feature/обновить стили чата

--- src/components/ChatConversation/ChatConversation.js
+++ src/components/ChatConversation/ChatConversation.js
@@ -1,21 +1,23 @@
+import React from 'react';
 import ChatRow from '../ChatRow/ChatRow';
 import { getObjectKey } from '../../utils/constants';
 
 function ChatConversation({ chatList, currentUser }) {
   return (
-    <div className="chat__body flex-grow-1 p-4">
-      {chatList.map((item, index) => (
-        <ChatRow
-          key={`${item.userKey}_${item.date}`}
-          index={index}
-          userKey={item.userKey}
-          userName={item.userName}
-          date={item.date}
-          message={item.message}
-          currentUser={getObjectKey(currentUser)}
-          chatRow={item}
-        />
-      ))}
+    <div className="chat__body flex-grow-1 position-relative overflow-hidden py-4">
+      <div className="chat__conversation position-absolute px-4">
+        {chatList.map((item, index) => (
+          <ChatRow
+            key={`${item.userKey}_${item.date}`}
+            index={index}
+            userKey={item.userKey}
+            userName={item.userName}
+            date={item.date}
+            message={item.message}
+            currentUser={getObjectKey(currentUser)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/ChatRow/ChatRow.js
+++ src/components/ChatRow/ChatRow.js
@@ -1,25 +1,34 @@
-import {
-  Col,
-} from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Clock } from 'react-bootstrap-icons';
 
 function ChatRow({ userKey, userName, date, message, currentUser }) {
   const userAvatar = Boolean(userName) ? userName.split('')[0] : '';
 
+  function formatDateValue(value) {
+    return value < 10 ? `0${value}` : value;
+  }
+
+  function formatDate() {
+    const messDate = new Date(date);
+    return `${formatDateValue(messDate.getDate())}.${formatDateValue(messDate.getMonth() + 1)}.${messDate.getFullYear()} ${formatDateValue(messDate.getHours())}:${formatDateValue(messDate.getMinutes())}:${formatDateValue(messDate.getSeconds())}`;
+  }
+
   return (
-    <div className={`d-flex justify-content-${userKey === currentUser ? 'end' : 'start'} mb-4`}>
-      <Col md={6} xl={4} className='chat__message d-flex px-0'>
-        <div className={`d-flex flex-column justify-content-end order-${userKey === currentUser ? 2 : 1}`}>
-          <span className={`user-avatar d-flex flex-column justify-content-center align-items-center fs-5 fw-semibold rounded-circle p-2 ${userKey !== currentUser ? 'user-avatar_inverted me-3' : 'ms-3'}`}>{userAvatar}</span>
+    <Col md={8} className={`chat__message fs-6 d-flex ${userKey === currentUser && 'justify-content-end offset-md-4'} px-0 mb-4`}>
+      <div className={`d-flex flex-column justify-content-end order-${userKey === currentUser ? 2 : 1}`}>
+        <span className={`user-avatar d-flex flex-column justify-content-center align-items-center fs-5 fw-semibold rounded-circle p-2 ${userKey !== currentUser ? 'user-avatar_inverted me-3' : 'ms-3'}`}>{userAvatar}</span>
+      </div>
+      <div className={`d-flex flex-column order-${userKey === currentUser ? 1 : 2}`}>
+        <div className={`chat__message-bubble px-4 py-3 mb-4 ${userKey === currentUser && 'chat__message-bubble_inverted text-dark'}`}>
+          <div className="chat__message-text mb-3">{message}</div>
+          <span className={`chat__message-date d-flex align-items-center ${userKey === currentUser ? 'text-muted' : 'justify-content-end text-light'}`}>
+            <Clock className={`me-1 ${userKey === currentUser && 'text-dark'}`} />
+            {formatDate()}
+          </span>
         </div>
-        <div className={`d-flex flex-column flex-grow-1 order-${userKey === currentUser ? 1 : 2}`}>
-          <div className={`chat__message-bubble p-3 mb-2 ${userKey === currentUser && 'chat__message-bubble_inverted text-dark'}`}>
-            <div className="mb-2">{message}</div>
-            <span>{date}</span>
-          </div>
-          {userName}
-        </div>
-      </Col>
-    </div>
+        <span className={`chat__message-username ${userKey === currentUser && 'text-end'}`}>{userName}</span>
+      </div>
+    </Col>
   );
 }
 
